a2563b4c3a015405b504ac1efcde5b9a958714bc
Robson Amorim <robson.amorim@lambda3.com.br>
2020-03-04 14:06:49 -0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import React, { useState } from 'react';
-import './App.css';
-import { HubConnectionBuilder } from '@aspnet/signalr';
+import React, { useState } from "react";
+import "./App.css";
+import { HubConnectionBuilder } from "@aspnet/signalr";
 
 function App() {
-
   const [idade, setIdade] = useState("?");
 
   let connection = new HubConnectionBuilder()
-    .withUrl("http://suaidadeserver.azurewebsites.net/faceHub")
+    .withUrl("https://suaidadeserver.azurewebsites.net/faceHub")
     .build();
 
-  connection.on('IdentificarIdade', (idade) => {
+  connection.on("IdentificarIdade", idade => {
     console.log(idade);
-    setIdade(idade)
+    setIdade(idade);
   });
 
-  connection.start()
-  .then(() => connection.invoke("IdentificarIdade", "Hello"));
+  connection.start().then(() => connection.invoke("IdentificarIdade", "Hello"));
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://media.giphy.com/media/XujzWKelSd5S/giphy.gif' alt="gif" />
-        <p>
-          Sua idade 
-        </p>
-        <p className="App-idade">
-        {idade}
-        </p>
-        <p>
-        ¯\_(ツ)_/¯
-        </p>
+        <img
+          src="https://media.giphy.com/media/XujzWKelSd5S/giphy.gif"
+          alt="gif"
+        />
+        <p>Sua idade</p>
+        <p className="App-idade">{idade}</p>
+        <p>¯\_(ツ)_/¯</p>
       </header>
     </div>
   );
